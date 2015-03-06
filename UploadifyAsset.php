2ff17bbb6c7f89b1cd22d5e185941152dd3dc6f2
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:23:25 +0800

    update

--- UploadifyAsset.php
+++ UploadifyAsset.php
@@ -6,25 +6,10 @@ use yii\web\AssetBundle;
 
 class UploadifyAsset extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-uploadify-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $css = ['uploadify.css'];
-    public $js = [];
+    public $js = ['jquery.uploadify.js'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    private function getJs() {
-        return [
-            YII_DEBUG ? 'jquery.uploadify.js' : 'jquery.uploadify.min.js',
-        ];
-    }
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        if (empty($this->js)) {
-            $this->js = $this->getJs();
-        }
-        return parent::init();
-    }
 
 }
