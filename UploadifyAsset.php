b28d062ab62d11cd816597e3bd75ebabe76856c8
xjflyttp <xjflyttp@gmail.com>
2017-11-30 11:15:37 +0800

    fix basePath

--- UploadifyAsset.php
+++ UploadifyAsset.php
@@ -7,7 +7,6 @@ use yii\web\AssetBundle;
 class UploadifyAsset extends AssetBundle {
 
     public $sourcePath = '@vendor/xj/yii2-uploadify-widget/assets';
-    public $basePath = '@webroot/assets';
     public $css = ['uploadify.css'];
     public $js = ['jquery.uploadify.js'];
     public $depends = ['yii\web\JqueryAsset'];
